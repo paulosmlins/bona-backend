71b5facd9ea4c5f400acf50b5cd5e1caf83182a4
Paulo Lins <paulosilas02@hotmail.com>
2022-04-11 09:55:07 -0300

    Criando service index, definindo a port e error

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const routes = require('./routes.js')
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({ error: error.message })
+})
+
+app.listen(3333, () => console.log("🦄 Server started!"))
